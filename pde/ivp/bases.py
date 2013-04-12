5d71b751dd7ad671cbf842925829e981bb41bce1
Keaton J. Burns <keaton.burns@gmail.com>
2013-04-12 22:37:58 +0100

    Move chebyshev deriv recurrence relation to a Numba function

--- pde/ivp/bases.py
+++ pde/ivp/bases.py
@@ -1,5 +1,6 @@
 """
 Spectral basis sets.
+Optimized using Numba.
 
 Author: Keaton J. Burns <keaton.burns@gmail.com>
 
@@ -8,6 +9,7 @@ Author: Keaton J. Burns <keaton.burns@gmail.com>
 
 import numpy as np
 import scipy.fftpack as fft
+from numba import double, jit, int_, void
 
 
 class _SpectralBase(object):
@@ -64,13 +66,14 @@ class FourierSeries(_SpectralBase):
 
     pass
 
+
 class ChebyshevSeries(_SpectralBase):
     """Chebyshev polynomial series."""
 
     def __init__(self, size, grid='extrema'):
 
         # Store inputs
-        self.size = size
+        self.size = {'xspace': size, 'kspace': size}
 
         # Setup tranforms
         if grid == 'extrema':
@@ -88,13 +91,14 @@ class ChebyshevSeries(_SpectralBase):
         self.xdata = np.zeros(size)
         self.kdata = np.zeros(size)
         self._deriv = np.zeros(size)
+        self._math = np.zeros(size)
 
         self.current_space = 'xspace'
         self.data = self.xdata
 
     def _extrema_grid(self):
 
-        N = self.size - 1
+        N = self.size['xspace'] - 1
         i = np.arange(N + 1)
         x = np.cos(np.pi * i / N)
 
@@ -103,7 +107,7 @@ class ChebyshevSeries(_SpectralBase):
     def _extrema_fwd(self):
 
         self.kdata[:] = fft.dct(self.xdata, type=1, norm=None)
-        self.kdata /= self.size - 1.
+        self.kdata /= self.size['xspace'] - 1.
         self.kdata[0] /= 2.
         self.kdata[-1] /= 2.
 
@@ -114,7 +118,7 @@ class ChebyshevSeries(_SpectralBase):
 
     def _roots_grid(self):
 
-        N = self.size
+        N = self.size['xspace']
         i = np.arange(N)
         x = np.cos(np.pi * (2.*i + 1.) / (2.*N))
 
@@ -123,7 +127,7 @@ class ChebyshevSeries(_SpectralBase):
     def _roots_fwd(self):
 
         self.kdata[:] = fft.dct(self.xdata, type=2, norm=None)
-        self.kdata /= self.size
+        self.kdata /= self.size['xspace']
         self.kdata[0] /= 2.
 
     def _roots_bwd(self):
@@ -134,17 +138,22 @@ class ChebyshevSeries(_SpectralBase):
     def differentiate(self, p):
 
         self.require_space('kspace')
-        a = self.kdata
-        b = self._deriv
-        N = self.size - 1
+        self._math[:] = self.kdata
+        N = self.size['kspace'] - 1
+        chebyshev_diff(self._math, self._deriv, p, N)
 
-        for derivative in xrange(p):
-            b[N] = 0.
-            b[N-1] = 2 * N * a[N]
-            for i in xrange(N-2, 0, -1):
-                b[i] = 2 * (i+1) * a[i+1] + b[i+2]
-            b[0] = a[1] + b[2] / 2.
-            a = b
+        return self._deriv
 
-        return b
+
+@jit(void(double[:], double[:], int_, int_))
+def chebyshev_diff(a, b, p, N):
+
+    for d in xrange(p):
+        b[N] = 0.
+        b[N-1] = 2. * N * a[N]
+        for i in xrange(N-2, 0, -1):
+            b[i] = 2 * (i+1) * a[i+1] + b[i+2]
+        b[0] = a[1] + b[2] / 2.
+        if d < p - 1:
+            a[:] = b
 
